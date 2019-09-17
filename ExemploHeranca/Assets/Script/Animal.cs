3e23e47ed913f39e1892918f3c63bb96fe25dc98
nask-douglas <naskdouglas@gmail.com>
2019-09-17 17:30:01 -0300

    .

---
+++ ExemploHeranca/Assets/Script/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] protected float velocidadeVertical;
+
+    Vector3 posicaoInicial;
+    Vector3 posicao;
+    private void Start()
+    {
+        posicaoInicial = transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    protected virtual void Move()
+    {
+        posicao = posicaoInicial + Vector3.right * Mathf.Sin(Time.time);
+        velocidadeVertical -= 14 * Time.deltaTime;
+
+        if (velocidadeVertical < 0)
+            velocidadeVertical = 0;
+
+        posicao.y = velocidadeVertical + posicao.y;
+        transform.position = posicao;
+    }
+
+    protected virtual void Fala()
+    {
+        Debug.Log("O Animal diz: Bem Vindo");
+    }
+
+    protected virtual void Pula()
+    {
+        velocidadeVertical = 2.0f;
+    }
+}
---
+++ ExemploHeranca/Assets/Script/Passaro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passaro : Animal
+{
+    [SerializeField] string nome;
+
+    public Passaro()
+    {
+        velocidadeVertical = 5f;
+    }
+    protected override void Fala()
+    {
+        Debug.Log("Pássaro:piu-piu");
+    }
+    protected override void Move()
+    {
+        base.Move();
+        transform.position += Vector3.up * Mathf.Sin(Time.time);
+    }
+}
